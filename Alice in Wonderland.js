176564ee6067a3b14505dd649d4176e4cf94d867
Dante Taviantz <taviantz@gmail.com>
2022-06-09 12:20:38 -0700

    Delete Alice in Wonderland.js

--- Alice in Wonderland.js
+++
@@ -1,6 +0,0 @@
-//The title of the book Alice's Adventures in Wonderland 
-//is often shortened to Alice in Wonderland.
-//Now, when we know how to print strings with quotes, we won't shorten it. 
-//Please print the title of the book in full.
-
-console.log("Alice's Adventures in Wonderland")
