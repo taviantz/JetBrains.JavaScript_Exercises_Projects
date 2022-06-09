37b58462fe360ffab9832376af6192b46c00d965
Dante Taviantz <taviantz@gmail.com>
2022-06-08 17:03:27 -0700

    We are including quotes in strings. These quotes are double and single ("" , '') quotations also with a backlash (\) to avoid confusion when the quotes are the same.

---
+++ Overview of the basic program.js/Printing quotes.js
@@ -0,0 +1,6 @@
+//We are including quotes in strings. 
+//These quotes are double and single ("" , '') quotations 
+//also with a backlash (\) to avoid confusion when the quotes are the same.  
+console.log("Yes, I'm ready to learn JS.");
+console.log("Yes, I\'m ready to learn JS.");
+console.log('Yes, I\'m ready to learn JS.');
