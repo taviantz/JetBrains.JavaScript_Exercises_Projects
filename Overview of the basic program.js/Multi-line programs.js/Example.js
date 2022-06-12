9a90ad342c75973d43d77a3539ca657604ebfe71
Dante Taviantz <taviantz@gmail.com>
2022-06-12 15:51:56 -0700

    The following code prints exactly three lines of text, each from a new line. Also a line break can also be done using the symbol \n

--- Overview of the basic program.js/Multi-line programs.js/Example.js
+++ Overview of the basic program.js/Multi-line programs.js/Example.js
@@ -1,3 +1,7 @@
+//The following code prints exactly three lines of text, each from a new line.
 console.log("I");
 console.log("know");
 console.log("JavaScript");
+
+//A line break can also be done using the symbol \n
+console.log("I\nknow\nJavaScript");
