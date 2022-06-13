6bbab8ef62806ebd2aedbea0e9d0c5eea2e03eab
Dante Taviantz <taviantz@gmail.com>
2022-06-13 13:58:53 -0700

    The console.log function also allows you to output an empty line without any information in it.

---
+++ Overview of the basic program.js/Multi-line programs.js/Empty line.js
@@ -0,0 +1,6 @@
+//The console.log function also allows you to output an empty line without any information in it.
+console.log("I");
+console.log();
+console.log("know");
+console.log();
+console.log("JavaScript");
