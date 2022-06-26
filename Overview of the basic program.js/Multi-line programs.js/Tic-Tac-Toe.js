6a4a39d4efa1f20e81f0353d26c65d75b959f121
Dante Taviantz <taviantz@gmail.com>
2022-06-25 17:59:25 -0700

    Writing a program to display the result grid of a Tic-Tac-Toe game.

---
+++ Overview of the basic program.js/Multi-line programs.js/Tic-Tac-Toe.js
@@ -0,0 +1,9 @@
+//You need to write a program to display the result grid of a Tic-Tac-Toe game.
+//The grid is shown below:
+//X X O
+//O X O
+//O O X
+
+console.log ("X X O");
+console.log ("O X O");
+console.log ("O O X");
