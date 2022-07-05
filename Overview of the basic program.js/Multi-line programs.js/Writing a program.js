3cc61d481469e42c26a7d706db6f800cd950f47a
Dante Taviantz <taviantz@gmail.com>
2022-07-04 20:48:32 -0700

    Add files via upload

---
+++ Overview of the basic program.js/Multi-line programs.js/Writing a program.js
@@ -0,0 +1,13 @@
+//Write a program that prints the following text:
+//How
+//deep
+//is
+//the
+//ocean?
+
+console.log("How");
+console.log("deep");
+console.log();
+console.log("is");
+console.log("the");
+console.log("ocean?");
