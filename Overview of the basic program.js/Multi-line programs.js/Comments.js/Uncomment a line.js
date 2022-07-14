3c981d39cf1813af664a58899e45ece6a2adf565
Dante Taviantz <taviantz@gmail.com>
2022-07-14 16:27:28 -0700

    Add comments and remove the unnecessary ones to print the string four.

---
+++ Overview of the basic program.js/Multi-line programs.js/Comments.js/Uncomment a line.js
@@ -0,0 +1,9 @@
+/*Add comments and remove the unnecessary ones to print the string four.
+**console.log("one");
+**console.log("One");
+**console.log("two");
+**console.log("two");
+**console.log("three"); 
+**console.log("four");
+console.log("four");*/
+console.log("four");
