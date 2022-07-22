011f25267d37c20644e5f9d68ffe803818197c3a
Dante Taviantz <taviantz@gmail.com>
2022-07-22 13:44:21 -0700

    Using comments by commenting and uncommenting for the right output.

---
+++ Comments.js/Comments.js
@@ -0,0 +1,12 @@
+//Using comments, make sure that the result of the output is as follows: 
+/*A cat
+says
+meow*/
+
+/*
+  console.log("A dog");*/
+  console.log("A cat");
+  console.log("says");
+  /*console.log("quack-quack");
+  console.log("woof");*/
+  console.log("meow");
