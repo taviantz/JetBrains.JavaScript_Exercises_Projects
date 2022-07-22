f1a80831ff9bb0e6d0bffde8372a5b770ca67fd8
Dante Taviantz <taviantz@gmail.com>
2022-07-22 13:35:04 -0700

    Comment the code so that the result of its output is the line I am learning JavaScript.

---
+++ Comments.js/I am learning JavaScript.js
@@ -0,0 +1,4 @@
+//Comment the code so that the result of its output is the line I am learning JavaScript.
+/*console.log("I am learning Java");*/
+console.log("I am learning JavaScript");
+/*console.log("I am learning C++");*/
